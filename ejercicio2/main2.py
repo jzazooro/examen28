18700dc3e815f828b77e022b48ca13960629fb15
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-10-30 03:05:42 +0100

    ejercicio2  casi terminado

--- ejercicio2/main2.py
+++ ejercicio2/main2.py
@@ -1,3 +1,6 @@
+from ejercicio2.pregunta2 import determinanterecursivo
 from pregunta2 import determinaterecursivo
+from pregunta2 import determinateiterativo
 
-determinaterecursivo()
+determinateiterativo()
+determinanterecursivo()
--- ejercicio2/pregunta2.py
+++ ejercicio2/pregunta2.py
@@ -1,4 +1,4 @@
-def determinaterecursivo():
+def determinateiterativo():
     a=float(input("Seleccione el elemento 1x1 de la matriz: "))
     b=float(input("Seleccione el elemento 1x2 de la matriz: "))
     c=float(input("Seleccione el elemento 1x3 de la matriz: "))
@@ -9,7 +9,40 @@ def determinaterecursivo():
     h=float(input("Seleccione el elemento 3x2 de la matriz: "))
     i=float(input("Seleccione el elemento 3x3 de la matriz: "))
     determinante = a*e*i + d*h*c + b*f*g - c*e*g -b*d*i - a*f*h
-    matriz=[[a, b, c], 
-    [d, e, f], 
-    [g, h, i]]
+    matriz=[[a, b, c], [d, e, f], [g, h, i]]
     print("El determinate de su matriz es: ", determinante)
+
+
+
+a=float(input("Seleccione el elemento 1x1 de la matriz: "))
+b=float(input("Seleccione el elemento 1x2 de la matriz: "))
+c=float(input("Seleccione el elemento 1x3 de la matriz: "))
+d=float(input("Seleccione el elemento 2x1 de la matriz: "))
+e=float(input("Seleccione el elemento 2x2 de la matriz: "))
+f=float(input("Seleccione el elemento 2x3 de la matriz: "))
+g=float(input("Seleccione el elemento 3x1 de la matriz: "))
+h=float(input("Seleccione el elemento 3x2 de la matriz: "))
+i=float(input("Seleccione el elemento 3x3 de la matriz: "))
+matriz=[[a, b, c], [d, e, f], [g, h, i]]
+def determinanterecursivo(matriz, i, parametro):
+    if i == 0:
+        parametro = matriz[0][0]*matriz[1][1]*matriz[2][2]+parametro
+        determinanterecursivo(matriz, i+1, parametro)
+    if i == 1:
+        parametro = matriz[2][0]*matriz[1][2]*matriz[0][1]+parametro
+        determinanterecursivo(matriz, i+1, parametro)
+    if i == 2:
+        parametro = matriz[1][0]*matriz[2][1]*matriz[0][2]+parametro
+        determinanterecursivo(matriz, i+1, parametro)
+    if i == 3:
+        parametro = parametro-matriz[0][2]*matriz[1][1]*matriz[2][0]
+        determinanterecursivo(matriz, i+1, parametro)
+    if i == 4:
+        parametro = parametro-matriz[0][1]*matriz[1][0]*matriz[2][2]
+        determinanterecursivo(matriz, i+1, parametro)
+    if i == 5:
+        parametro = parametro-matriz[0][0]*matriz[2][1]*matriz[1][2]
+        print("El determinante de su matriz es: ", parametro)
+determinanterecursivo(matriz, 0, 0)
+
+    
