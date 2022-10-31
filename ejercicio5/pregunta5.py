419670c2528a0b1769b11648ce94fa3e54d86faa
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-10-31 03:01:07 +0100

    ejercicio 5 en el lanzador y main

--- ejercicio5/pregunta5.py
+++ ejercicio5/pregunta5.py
@@ -4,6 +4,3 @@ def encriptar():
     contraseña=("Ayudame Obi Wan Kenobi, eres mi unica esperanza")
     contraseñaencriptada=hashlib.md5(contraseña.encode("utf-8")).hexdigest()
     print(contraseñaencriptada[:8])
-
-encriptar()
-
