91398c90d540613bcf932c07bb2e7b02b3754e08
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-10-31 12:48:15 +0100

    ejercicio 5

--- ejercicio5/pregunta5.py
+++ ejercicio5/pregunta5.py
@@ -1,10 +1,6 @@
 import hashlib
 
 def encriptar():
-    contraseña=("Ayudame Obi Wan Kenobi, eres mi unica esperanza")
+    contraseña=input("Que mensaje deseas mandar: ")
     contraseñaencriptada=hashlib.md5(contraseña.encode("utf-8")).hexdigest()
     print("su contraseña encriptada es: ", contraseñaencriptada[:8])
-
-def desencriptar():
-    contraseñaencriptada=("144412cf")
-    print("su contraseña desencriptada es: ", "Ayudame Obi Wan Kenobi, eres mi unica esperanza")
